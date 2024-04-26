2130b4334dc4f7000e41ed7c7b9e53b682cbdee2
vera branton <veronav77@gmail.com>
2024-04-26 01:27:05 -0400

    js  added

--- main.js
+++ main.js
@@ -1,4 +1,39 @@
+//global variables
 let operator = "";
 let previousValue = "";
 let currentValue = "";
 
+//storing all components
+document.addEventListener("DOMContentLoaded", function() {
+    let clear = document.querySelector("#clear-btn");
+    let equal = document.querySelector(".equal");
+    let decimal = document.querySelector(".decimal");
+    let numbers = document.querySelectorAll(".number");
+    let operators = document.querySelectorAll(".operator");
+    let previousScreen = document.querySelector(".previous");
+    let currentScreen = document.querySelector(".current");
+
+    numbers.forEach((number) => number.addEventListener("click",function(e){
+        handleNumber(e.target.textContent);
+        currentScreen.textContent = currentValue;
+    }))
+})
+    operators.forEach((op) => op.addEventListener("click", function(e) {
+        handleOperator(e.target.textContent)
+        previousScreen.textContent = previousVaule + "" + operator;
+        currentScreen.textContent = currentValue;
+    }))
+
+//html DOM Element textContent that will get the numbers 7, 8 9 and so on
+function handleNumber(num) {
+    //will limit the length of the current value
+    if(currentValue.length <= 5){ 
+        currentValue += num;
+    }
+}
+
+function handleOperator(op) {
+    operator = op;
+    previousValue = currentValue;
+    currentValue = '';
+}
